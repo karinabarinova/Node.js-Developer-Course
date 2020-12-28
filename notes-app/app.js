daa0f24387542d0d4eb53ab99c4d574d1b633647
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-28 16:54:17 +0200

    finished lesson
    Tested importing my own files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,12 @@
-const fs = require('fs');
+const getNotes = require('./notes');
+const notes = getNotes();
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!\n');
-fs.appendFileSync('append.txt', 'This text was added by appendFileSync()\n');
-fs.appendFileSync('append.txt', 'Previous text was not overwritten');
-fs.appendFileSync('notes.txt', 'This string is the result of appendFileSync()');
+console.log(notes);
+
+
+// const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'This file was created by Node.js!\n');
+// fs.appendFileSync('append.txt', 'This text was added by appendFileSync()\n');
+// fs.appendFileSync('append.txt', 'Previous text was not overwritten');
+// fs.appendFileSync('notes.txt', 'This string is the result of appendFileSync()');
