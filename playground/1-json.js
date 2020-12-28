000b473b79bcbf83536508b6108b62524c1931eb
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-28 20:02:10 +0200

    cleanup

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,21 +1,9 @@
 const fs = require('fs');
 
-// const book = {
-//     title: 'Ego is the Enemy',
-//     author: 'Ryan Holiday'
-// }
-
-// const bookJSON = JSON.stringify(book);
-// fs.writeFileSync('1-json.json', bookJSON);
-
-// const dataBuffer = fs.readFileSync('1-json.json'); //read the file and get binary data
-// const dataJSON = dataBuffer.toString(); // convert binary to a string
-// const data = JSON.parse(dataJSON); //parse json data to an object
-// console.log(data.author);
-
 const dataBuffer = fs.readFileSync('1-json.json');
 const dataJSON = dataBuffer.toString();
 const data = JSON.parse(dataJSON);
+
 data.name = 'Karina';
 data.age = 24;
 const newDataJSON = JSON.stringify(data);
