1cf71083974d422c6dcce1bd79f4ca85616f6ab5
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-28 19:18:47 +0200

    Parsed command line arguments
    Arguments parsing with Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,35 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
 
-console.log(chalk.green('Success'));
-console.log(chalk.green.inverse.bold("Some text"));
-console.log(chalk.red.underline('Another text'));
+yargs.version('1.1.0');
+yargs.command({
+    command: 'add',
+    description: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note');
+    }
+})
+yargs.command({
+    command: 'remove',
+    description: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note');
+    }
+})
+yargs.command({
+    command: 'list',
+    description: 'List your notes',
+    handler: function() {
+        console.log("Listing out all notes");
+    }
+})
+yargs.command({
+    command: 'read',
+    description: 'Read a note',
+    handler: function() {
+        console.log("Reading a note");
+    }
+})
+
+console.log(yargs.argv);
