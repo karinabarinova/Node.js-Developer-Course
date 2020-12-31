6666497dae297ca2435f26bd41bb4ab929f7ddf5
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-31 12:32:34 +0200

    callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,15 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error);
-    console.log('data', data);
+geocode(process.argv[2], (error, data) => {
+    if (error)
+        return console.log(error);
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error)
+            return console.log(error);
+
+        console.log(data.location);
+        console.log(forecastData);
+    })  
 });
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -2,7 +2,8 @@ const request = require('postman-request');
 
 const geocode = (address, callback) => {
     const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoia2Jhcmlub3ZhIiwiYSI6ImNramEyYTRneDdhM2wyeXFqbG9qb2MwcHIifQ.J-qnGRyD3GJmr6aQm597cg&limit=1';
- 
+    if (!address) 
+        return callback('Missing address', undefined);
     request({url: url, json: true}, (err, res) => {
         if (err)
             callback('Unable to connect to location services!', undefined);
@@ -10,8 +11,8 @@ const geocode = (address, callback) => {
             callback('Location not found, try another search', undefined);
         } else {
             callback(undefined, {
-                latitude: res.body.features[0].center[0],
-                longitude: res.body.features[0].center[1],
+                latitude: res.body.features[0].center[1],
+                longitude: res.body.features[0].center[0],
                 location: res.body.features[0].place_name
             });
         }
