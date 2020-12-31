50bfed01bb7ede506343839c085e7476eaeebcd9
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-31 15:18:33 +0200

    added dynamic pages with templating and customized the views directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,36 @@
 const path = require('path');
 const express = require('express');
 
-const publicDir = path.join(__dirname, '../public');
 const app = express();// new express application
+
+// Define paths for express congig
+const publicDir = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+//set up handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+//setup static directory serve
 app.use(express.static(publicDir));
-//app.com - root route
-//app.com/help - help route
-//app.com/about - about route
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Karina'
+    }); //renders a view
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Karina'
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: "How can I help you?"
+    });
+})
 
 app.get('/weather', (req, res) => {
     res.send([
