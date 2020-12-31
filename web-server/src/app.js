39c5ddb607f900ea8500d64d93a95c02b07a7c68
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-31 14:02:25 +0200

    serving up html and json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,19 +7,29 @@ const app = express();// new express application
 //app.com/about - about route
 
 app.get('/', (req, res) => { //res it to be set up to send back to a requestor
-    res.send('Hello express'); //send something back to the requestor
+    res.send('<h1>Weather</h1>'); //send something back to the requestor
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page');
+    res.send({
+        name: 'Karina',
+        age: 24
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.send('<h1>About page</h1>');
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page');
+    res.send([
+        {
+            location: 'New York'
+        },
+        {
+            forecast: 'Cloudy'
+        }
+    ]);
 })
 app.listen('3000', () => {
     console.log('Server is up on port 3000');
