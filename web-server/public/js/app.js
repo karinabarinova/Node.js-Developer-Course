c06f38c7ce3c827475fbd9a7acce10525210bfd2
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-01 13:18:36 +0200

    cleanup

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,9 +1,4 @@
 //browser HTTP requests with fetch
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     });
-// })
 
 fetch('http://localhost:3000/weather?address=boston').then((response) => {
     response.json().then((data) => {
