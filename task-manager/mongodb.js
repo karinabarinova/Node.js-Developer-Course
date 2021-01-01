5b2bf671de8be8f36c2d97c519392a7fc4af53cb
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-01 22:18:59 +0200

    the ObjectID lesson

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,9 +1,14 @@
 //CRUD - create read update and delete operations
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager'; //any name
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.toHexString().length);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error)
@@ -11,26 +16,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName);
     // db.collection('users').insertOne({
-    //     name: 'Karina',
-    //     age: 24
+    //     name: 'Vikram',
+    //     age: 27
     // }, (error, result) => {
     //     if (error)
     //         return console.log('Unable to insert user');
     //     console.log(result.ops);
     // }); // insert one user
-    db.collection('tasks').insertMany([
-        {
-        description: 'Finish homework',
-        completed: true
-    }, {
-        description: "Pay bills",
-        completed: false
-    }, {
-        description: 'Clean the bathroom',
-        completed: false
-    }], (error, result) => {
-        if (error)
-            return console.log('Unable to insert tasks');
-        console.log(result.ops);
-    })
+    // db.collection('tasks').insertMany([
+    //     {
+    //     description: 'Finish homework',
+    //     completed: true
+    // }, {
+    //     description: "Pay bills",
+    //     completed: false
+    // }, {
+    //     description: 'Clean the bathroom',
+    //     completed: false
+    // }], (error, result) => {
+    //     if (error)
+    //         return console.log('Unable to insert tasks');
+    //     console.log(result.ops);
+    // })//insert many
+
 })
