374f114e4faa442a49beb551434b449e1f46008c
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-01 22:02:15 +0200

    Inserted documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -10,8 +10,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
         return console.log('Unable to connect to database');
 
     const db = client.db(databaseName);
-    db.collection('users').insertOne({
-        name: 'Karina',
-        age: 24
-    });
+    // db.collection('users').insertOne({
+    //     name: 'Karina',
+    //     age: 24
+    // }, (error, result) => {
+    //     if (error)
+    //         return console.log('Unable to insert user');
+    //     console.log(result.ops);
+    // }); // insert one user
+    db.collection('tasks').insertMany([
+        {
+        description: 'Finish homework',
+        completed: true
+    }, {
+        description: "Pay bills",
+        completed: false
+    }, {
+        description: 'Clean the bathroom',
+        completed: false
+    }], (error, result) => {
+        if (error)
+            return console.log('Unable to insert tasks');
+        console.log(result.ops);
+    })
 })
