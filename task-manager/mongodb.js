ec9bf2c9c9410492592dc47236702d70737f7365
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-01 15:36:02 +0200

    task manager app, connected mongodb database

---
+++ task-manager/mongodb.js
@@ -0,0 +1,17 @@
+//CRUD - create read update and delete operations
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager'; //any name
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error)
+        return console.log('Unable to connect to database');
+
+    const db = client.db(databaseName);
+    db.collection('users').insertOne({
+        name: 'Karina',
+        age: 24
+    });
+})
