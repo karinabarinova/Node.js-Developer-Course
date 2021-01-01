ab3a950e7df8a7a6572fc058544deb689f0ddd6d
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-01 22:42:26 +0200

    Querying documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,42 +1,40 @@
 //CRUD - create read update and delete operations
-
 const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager'; //any name
-
-const id = new ObjectID();
-console.log(id);
-console.log(id.toHexString().length);
-console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error)
         return console.log('Unable to connect to database');
 
     const db = client.db(databaseName);
-    // db.collection('users').insertOne({
-    //     name: 'Vikram',
-    //     age: 27
-    // }, (error, result) => {
-    //     if (error)
-    //         return console.log('Unable to insert user');
-    //     console.log(result.ops);
-    // }); // insert one user
-    // db.collection('tasks').insertMany([
-    //     {
-    //     description: 'Finish homework',
-    //     completed: true
-    // }, {
-    //     description: "Pay bills",
-    //     completed: false
-    // }, {
-    //     description: 'Clean the bathroom',
-    //     completed: false
-    // }], (error, result) => {
-    //     if (error)
-    //         return console.log('Unable to insert tasks');
-    //     console.log(result.ops);
-    // })//insert many
+    
+    //read/find/findOne data from the database
+    db.collection('users').findOne({_id: new ObjectID('5fef828cf4e6e11fdc9f52ae')}, (error, user) => {
+        if (error)
+            return console.log('Unable to fetch');
+        console.log(user);
+    })
 
+    db.collection('users').find({age: 24}).toArray((error, users) => {
+        if (error)
+            return console.log('Unable to fetch users');
+        console.log(users);
+    })
+    db.collection('users').find({age: 24}).count((error, count) => {
+        if (error)
+            return console.log('Unable to count users');
+        console.log(count);
+    })
+    db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
+        if (error)
+            console.log('Unable to fetch tasks');
+        console.log(tasks);
+    })
+    db.collection('tasks').findOne({_id: new ObjectID("5fef7f4aaf51710ec0faabc2")}, (error, task) => {
+        if (error)
+            return console.log('Unable to fetch task');
+        console.log(task);
+    })
 })
