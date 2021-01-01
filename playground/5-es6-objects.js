d78d26742b75fcf846d11c3f6b764fd91616b393
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-01 13:18:09 +0200

    default parameters and browser http requests with fetch

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -28,7 +28,7 @@ const product = {
 // console.log(price);
 // console.log(rating);
 
-const transaction = (type, { label, stock }) => {
+const transaction = (type, { label = '', stock = 0 } = {}) => {
     console.log(type, label, stock);
 }
 
--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,15 @@
-console.log('Client side js file is loaded');
+//browser HTTP requests with fetch
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     });
+// })
+
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.log(data.error);
+        }
+        console.log(data.location, data.forecast, data.address);
+    })
+})
