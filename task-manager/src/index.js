44cdd3c3e6466754bb0e8cfc5ad382f37c7b277a
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-02 20:50:28 +0200

    resource deleting endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -116,6 +116,33 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+//deleting endpoints
+app.delete('/users/:id', async(req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const deletedUser = await User.findByIdAndDelete(_id);
+        if (!deletedUser)
+            return res.status(404).send();
+        res.send(deletedUser);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const deletedTask = await Task.findByIdAndDelete(_id);
+        if (!deletedTask)
+            return res.status(404).send();
+        res.send(deletedTask);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
