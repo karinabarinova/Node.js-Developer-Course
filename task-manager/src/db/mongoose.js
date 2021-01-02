726a86122f6291c64b51f72bdd5269bbf18b6eb6
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-02 17:16:16 +0200

    resource creation endpoints part2

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -6,14 +6,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true
 })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
