a92e695c4cb584c268e77c95cc5369cfb349b3c4
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-02 13:27:40 +0200

    Updating documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -10,31 +10,26 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName);
     
-    //read/find/findOne data from the database
-    db.collection('users').findOne({_id: new ObjectID('5fef828cf4e6e11fdc9f52ae')}, (error, user) => {
-        if (error)
-            return console.log('Unable to fetch');
-        console.log(user);
-    })
-
-    db.collection('users').find({age: 24}).toArray((error, users) => {
-        if (error)
-            return console.log('Unable to fetch users');
-        console.log(users);
-    })
-    db.collection('users').find({age: 24}).count((error, count) => {
-        if (error)
-            return console.log('Unable to count users');
-        console.log(count);
-    })
-    db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
-        if (error)
-            console.log('Unable to fetch tasks');
-        console.log(tasks);
-    })
-    db.collection('tasks').findOne({_id: new ObjectID("5fef7f4aaf51710ec0faabc2")}, (error, task) => {
-        if (error)
-            return console.log('Unable to fetch task');
-        console.log(task);
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID('5fef7b8828417b66344981b4')
+    // }, {
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }).then((result) => {
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error);
+    // })
+    db.collection('tasks').updateMany({
+        completed: false
+    },{
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result.modifiedCount);
+    }).catch((error) => {
+        console.log(error);
     })
 })
