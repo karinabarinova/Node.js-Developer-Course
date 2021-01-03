90767a99eab9676408354a9ea48a57fbe15ff306
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-03 12:25:34 +0200

    express middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled');
+//     } else {
+//         next();
+//     }
+// }) //middleware
+
+app.use((req, res, next) => {
+    res.status(503).send('Website is under maintenance');
+})
+
 app.use(express.json()); //automatically parse 
 //incoming json to an object to access it in our request handlers
 //req.body
