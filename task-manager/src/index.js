af9842066dad9041e15d0f237a112550a3f2b7b7
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-03 16:14:07 +0200

    working with timestamps

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -16,19 +16,19 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
 
-const Task = require('./models/task');
-const User = require('./models/user');
+// const Task = require('./models/task');
+// const User = require('./models/user');
 
-const main = async () => {
-    // const task = await Task.findById('5ff1b3e661beb44b709efde9');
-    // await task.populate('owner').execPopulate();
-    // console.log(task.owner);
+// const main = async () => {
+//     // const task = await Task.findById('5ff1b3e661beb44b709efde9');
+//     // await task.populate('owner').execPopulate();
+//     // console.log(task.owner);
 
-    const user = await User.findById('5ff1b3cc61beb44b709efde6');
-    await user.populate('tasks').execPopulate();
-    console.log(user.tasks)
+//     const user = await User.findById('5ff1b3cc61beb44b709efde6');
+//     await user.populate('tasks').execPopulate();
+//     console.log(user.tasks)
 
 
-}//relationshio between two models
+// }//relationshio between two models
 
-main();
+// main();
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -45,6 +45,8 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
+}, {
+    timestamps: true
 });
 
 //virtual property/ relationship between models
