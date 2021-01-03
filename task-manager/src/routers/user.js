f35f816c94cb7cf247e38c6384647d667a5f3d27
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-03 18:32:43 +0200

    Validation challenge

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -8,6 +8,12 @@ const upload = multer({
     dest: 'avatars',
     limits: {
         fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/))
+            return cb(new Error('Please upload an image file'));
+        
+        cb(undefined, true);
     }
 });
 
