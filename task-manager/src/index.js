b44510490308437f99f976b2b1577692d541b1d5
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-01-03 19:49:33 +0200

    Auto-cropping and Image Formatting

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,34 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-//add file upload to express
-const multer = require('multer');
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        // if (!file.originalname.endsWith('.pdf'))
-        //     return cb(new Error('Please upload a PDF'))
-        if (!file.originalname.match(/\.(doc|docx)$/))
-            return cb(new Error('Please upload a Word document'))
-        cb(undefined, true);
-        // cb(new Error(''), false)
-        // cb(undefined, true)
-    }
-});
-const errorMiddleware = (req, res, next) => {
-    throw new Error('From my middleware');
-}
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send();
-}, (error, req, res, next) => {//function set up to handle uncaught errors!
-    res.status(404).send({error: error.message});
-})
-
 app.use(express.json()); //automatically parse 
 //incoming json to an object to access it in our request handlers
 //req.body
